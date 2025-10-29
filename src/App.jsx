abc9f594a6f12c7f420b7411a61c2479de55cd5e
Aladdin <aladdin90.se@gmail.com>
2025-10-29 17:20:27 +0100

    Fix routing for GitHub Pages - switch from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import './assets/css/main.css';
 import './assets/css/noscript.css';
 import profileImg from './images/profile bild.jpeg';
