c39df02939c65a14be2c9804645e8ae1bdabad61
Aladdin <aladdin90.se@gmail.com>
2025-11-25 12:32:17 +0100

    fixat desigenen i skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,31 +14,31 @@ function Skills() {
           <section>
             <div className='mySkills'>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-blazor-original"></i></div>
+                <div className='skillsIcon'><i className="fa-solid fa-code fa-2xl"></i></div>
                 <h3>Blazor</h3>
               </div>              
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="fa-brands fa-html5 fa-2xl"></i></div>
+                <div className='skillsIcon'><i className="fa-brands fa-html5 fa-2xl"></i></div>
                 <h3>HTML</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="fa-brands fa-css3-alt fa-2xl"></i></div>
+                <div className='skillsIcon'><i className="fa-brands fa-css3-alt fa-2xl"></i></div>
                 <h3>CSS</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="fa-brands fa-html5 fa-2xl"></i></div>
+                <div className='skillsIcon'><i className="fa-brands fa-js-square fa-2xl"></i></div>
                 <h3>Javascript</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="fa-brands fa-react fa-2xl"></i></div>
+                <div className='skillsIcon'><i className="fa-brands fa-react fa-2xl"></i></div>
                 <h3>React</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-typescript-plain"></i></div>
+                <div className='skillsIcon'><i className="devicon-typescript-plain fa-2xl"></i></div>
                 <h3>TypeScript</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-tailwindcss-original"></i></div>
+                <div className='skillsIcon'><i className="fa-solid fa-wind fa-2xl"></i></div>
                 <h3>Tailwind CSS</h3>
               </div>
             </div>
@@ -48,34 +48,35 @@ function Skills() {
 
           <section>
             <div className='mySkills'>
-              <div className='skillsBox'><div className='skillsIcon'><i class="devicon-csharp-plain"></i></div>
+              <div className='skillsBox'>
+                <div className='skillsIcon'><i className="devicon-csharp-plain fa-2xl"></i></div>
                 <h3>C#</h3>
               </div>              
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-dotnetcore-plain"></i></div>
+                <div className='skillsIcon'><i className="devicon-dotnetcore-plain fa-2xl"></i></div>
                 <h3>ASP.NET Core</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-graphql-plain"></i></div>
+                <div className='skillsIcon'><i className="devicon-graphql-plain fa-2xl"></i></div>
                 <h3>GraphQL</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-nodejs-plain"></i></div>
+                <div className='skillsIcon'><i className="devicon-nodejs-plain fa-2xl"></i></div>
                 <h3>Node.js</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-azuresqldatabase-plain"></i></div>
+                <div className='skillsIcon'><i className="fa-solid fa-server fa-2xl"></i></div>
                 <h3>SQL Server</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="fa-solid fa-database"></i></div>
+                <div className='skillsIcon'><i className="fa-solid fa-database fa-2xl"></i></div>
                 <h3>Entity Framework</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-firebase-plain"></i></div>
+                <div className='skillsIcon'><i className="devicon-firebase-plain fa-2xl"></i></div>
                 <h3>Firebase</h3>
               </div>
-            </div>
+            </div>  
           </section>
 
           <h3 className='skillsTitle'>Other Technologies</h3>
@@ -83,19 +84,19 @@ function Skills() {
           <section>
             <div className='mySkills'>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="fa-brands fa-github fa-2xl"></i></div>
+                <div className='skillsIcon'><i className="fa-brands fa-github fa-2xl"></i></div>
                 <h3>GitHub</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="fa-brands fa-umbraco"></i></div>
+                <div className='skillsIcon'><i className="fa-brands fa-umbraco fa-2xl"></i></div>
                 <h3>Umbraco</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="fa-brands fa-figma fa-2xl"></i></div>
+                <div className='skillsIcon'><i className="fa-brands fa-figma fa-2xl"></i></div>
                 <h3>Figma</h3>
               </div>
               <div className='skillsBox'>
-                <div className='skillsIcon'><i class="devicon-azure-plain"></i></div>
+                <div className='skillsIcon'><i className="devicon-azure-plain fa-2xl"></i></div>
                 <h3>Azure</h3>
               </div>
             </div>
